3c720ea6a836e6bc0e74a12a97fcf3e52811e874
Luis Quijada Muñoz <l.quijada@duoc.cl>
2025-05-12 21:22:21 -0400

    primer commit del proyecto

---
+++ src/test/java/cl/duoc/msvc_productos/MsvcProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.msvc_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
